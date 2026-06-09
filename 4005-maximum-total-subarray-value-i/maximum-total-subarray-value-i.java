40d2ef3962e04def31e1b3908982e4e0f29e6fae
abhishekraghav-05 <abhishek.raghav89200@gmail.com>
2026-06-09 20:47:45 +0530

    Time: 1 ms (100.00%) | Memory: 62.1 MB (78.42%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int num : nums) {
+            max = Math.max(num, max);
+            
+            min = Math.min(num, min);
+        }
+        int difference = max - min;
+        return (long) k * difference;
+    }
+}
