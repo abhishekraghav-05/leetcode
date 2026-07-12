7590e4a89dde4974da7079a978b331ef277e2fae
abhishekraghav-05 <abhishek.raghav89200@gmail.com>
2026-07-12 18:45:50 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (75.58%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.java
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.java
@@ -2,7 +2,6 @@ class Solution {
     public int secondsBetweenTimes(String startTime, String endTime) {
         int num1 = Integer.parseInt(startTime.substring(6,8));
         int num2 = Integer.parseInt(endTime.substring(6,8));
-
         int ans1 = 0;
         if(num2 < num1) {
             ans1 = (num2 + 60) - num1;
@@ -13,14 +12,12 @@ class Solution {
 
         int num3 = Integer.parseInt(startTime.substring(3,5));
         int num4 = Integer.parseInt(endTime.substring(3,5));
-
         int min = 0, ans2 = 0;
         if(num4 < num3) {
             min = (num4 + 60) - num3;
         }else {
             min = num4 - num3;
         }
-
         if(num2 < num1) {
             ans2 = (min - 1) * 60;
         }else {
@@ -31,7 +28,6 @@ class Solution {
         int num5 = Integer.parseInt(startTime.substring(0, 2));
         int num6 = Integer.parseInt(endTime.substring(0, 2));
         int hour = num6 - num5;
-        
         int ans3 = 0;
         if(num4 < num3) {
             ans3 = (hour - 1) * 60 * 60;
